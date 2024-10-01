8f520d6368c36aa5bc57ece232b3e0d78cac7183
Diktator <dychervony@gmail.com>
2024-10-01 15:45:58 +0300

    Сделал практику за 01.10.2024

--- UserHash/src/index.js
+++ UserHash/src/index.js
@@ -2,10 +2,12 @@ import express from "express";
 import exphbs from "express-handlebars";
 import cookieParser from "cookie-parser";
 import path from "node:path";
+import session from "express-session";
 import "dotenv/config";
 import { siteRoutes } from "./routes/site-routes.js";
 import { userRoutes } from "./routes/user-routes.js";
 import { checkUser } from "./middlewares/checkuser-middleware.js";
+import { accessControl } from './middlewares/accesscontrol-middleware.js';
 
 const PORT = process.env.PORT || 3000, server = express();
 const hbs = exphbs.create({
@@ -14,7 +16,15 @@ const hbs = exphbs.create({
 });
 
 server.use(cookieParser());
+server.use(
+    session({
+        secret: process.env.SESSION_KEY,
+        resave: false,
+        saveUninitialized: false,
+    })
+);
 server.use(checkUser);
+server.use(accessControl);
 server.use(express.json());
 server.use(express.urlencoded({ extended: true }));
 server.use(express.static("public"));
@@ -22,7 +32,7 @@ server.engine("hbs", hbs.engine);
 server.set("view engine", "hbs");
 server.set("views", path.join("src", "views"));
 server.use(siteRoutes);
-server.use(userRoutes);
+server.use("/user", userRoutes);
 server.listen(PORT, () =>
     console.log(`Сервер работает на http://localhost:${PORT}`)
 );
--- UserHash/src/routes/user-routes.js
+++ UserHash/src/routes/user-routes.js
@@ -1,43 +1,30 @@
 import { Router } from "express";
-import bcrypt from "bcrypt";
-import validator from "validator";
 
 export const userRoutes = Router();
 
-userRoutes.post("/user/signup", async (req, res) => {
-    const { login, email, password, confirm_password } = req.body;
-
-    if (!validator.isEmail(email)) return res.status(400).send("Неправильный формат email.");
-    if (!validator.isLength(password, { min: 6 })) return res.status(400).send("Пароль должен быть не менее 6 символов.");
-    if (password !== confirm_password) return res.status(400).send("Пароли не совпадают.");
-
-    const hashedPassword = await bcrypt.hash(password, 10);
-    users.push({
-        login,
-        email,
-        password: hashedPassword,
-    });
-    res.cookie("user", login, {
-        httpOnly: true,
-        maxAge: 1000000,
-    });
+userRoutes.route("/signin").get((req, res) => {
+    res.render("form_auth"); 
+}).post(authUser, (req, res) => {
+    req.session.user = {
+        login: req.body.login,
+        email: req.body.email,
+    };
     res.redirect("/");
 });
-userRoutes.post("/user/signin", async (req, res) => {
-    const { login, password } = req.body;
-
-    const user = users.find(u => u.login === login);
-    if (!user) return res.status(400).send("Пользователь не найден.");
-    const isPasswordMatch = await bcrypt.compare(password, user.password);
-    if (!isPasswordMatch) return res.status(400).send("Неправильный пароль.");
-
-    res.cookie("user", login, {
-        httpOnly: true,
-        maxAge: 1000000,
-    });
+userRoutes.get("/", (req, res) => {
+    res.json(users);
+    res.end();
+});
+userRoutes.route("/signup").get((req, res) => {
+    res.render("form_register");
+}).post(createUser, (req, res) => {
+    req.session.user = {
+        login: req.body.login,
+        email: req.body.email,
+    };
     res.redirect("/");
 });
-userRoutes.get("/user/logout", (req, res) => {
-    res.clearCookie("user");
+userRoutes.get("/logout", (req, res) => {
+    req.session.destroy();
     res.redirect("/");
 });
