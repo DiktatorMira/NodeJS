4af7887c7521fe5fd7035440b3a30b0674a4a803
DiktatorMira <112933359+DiktatorMira@users.noreply.github.com>
2024-12-31 17:34:58 +0200

    Немного новогоднего фикса багов в проекте.

--- RestApiProj/src/controllers/passReset-controller.ts
+++ RestApiProj/src/controllers/passReset-controller.ts
@@ -1,4 +1,5 @@
 import { Request, Response } from 'express';
+import { User } from '../models/user-model';
 import { PasswordResetService } from '../services/passReset-service';
 import { PasswordReset } from '../models/passwordReset-model';
 
@@ -6,15 +7,19 @@ export class PasswordResetController {
     static async requestPasswordReset(req: Request, res: Response): Promise<any> {
         const { email } = req.body;
         try {
+            const user = await User.findOne({ where: { email } });
+            if (!user) return res.status(404).json({ message: 'Пользователь с таким email не найден' });
             await PasswordResetService.sendPasswordResetEmail(email);
             return res.status(200).json({ message: 'Письмо для сброса пароля отправлено' });
         } catch (error) {
-            return res.status(500).json({ message: 'Ошибка отправки письма для сброса пароля: ', error });
+            return res.status(500).json({ message: 'Ошибка отправки письма для сброса пароля', error });
         }
     }
     static async resetPassword(req: Request, res: Response): Promise<any> {
         const { token, newPassword } = req.body;
         try {
+            const passReset = await PasswordReset.findOne({ where: { token } });
+            if (!passReset || new Date(passReset.end_date) < new Date())  throw new Error('Токен недействителен или истёк');
             await PasswordResetService.resetPassword(token, newPassword);
             return res.status(200).json({ message: 'Пароль сброшен' });
         } catch (error) {
@@ -22,8 +27,12 @@ export class PasswordResetController {
         }
     }
     static async create(req: Request, res: Response) {
-        const passReset = await PasswordReset.create(req.body);
-        res.status(201).json(passReset);
+        try {
+            const passReset = await PasswordReset.create(req.body);
+            res.status(201).json(passReset);
+        } catch (error) {
+            res.status(500).json({ message: 'Ошибка при создании записи', error });
+        }
     }
     static async getAll(req: Request, res: Response) {
         const passReset = await PasswordReset.findAll();
@@ -34,11 +43,11 @@ export class PasswordResetController {
         passReset ? res.json(passReset) : res.status(404).json({ message: 'Хешированный пароль не найден!' });
     }
     static async update(req: Request, res: Response) {
-        const [updated] = await PasswordReset.update(req.body, { where: { id: req.params.id } });
+        const [updated] = await PasswordReset.update(req.body, { where: { reset_id: req.params.id } });
         updated ? res.json(await PasswordReset.findByPk(req.params.id)) : res.status(404).json({ message: 'Хешированный пароль не найден!' });
     }
     static async delete(req: Request, res: Response) {
-        const deleted = await PasswordReset.destroy({ where: { id: req.params.id } });
+        const deleted = await PasswordReset.destroy({ where: { reset_id: req.params.id } });
         deleted ? res.status(204).send() : res.status(404).json({ message: 'Хешированный пароль не найден!' });
     }
 }
